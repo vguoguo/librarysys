c6fd206e83a62bc79dd821857c9b351aec61db98
abdelillahfaddaoui <abdelillah.faddaoui@gmail.com>
2016-01-19 17:42:32 -0600

    test for the data access

--- jazsystem/src/DataAccess/Main.java
+++ jazsystem/src/DataAccess/Main.java
@@ -1,7 +1,10 @@
 package DataAccess;
 
+import Application.Author;
 import Application.Book;
 import Application.BookCopy;
+import Application.Member;
+import Application.Periodical;
 
 public class Main {
 
@@ -12,6 +15,19 @@ book.addBookCopy(new BookCopy());
 BookDataAccess bda=new BookDataAccess();
 bda.saveBook(1, book);
 System.out.println(bda.readBook(1).getTitle()+"  is teh title ");
+
+Member member=new Member();
+MemberDataAccess MDA =new MemberDataAccess();
+MDA.saveMember(1, member);
+
+Author author =new Author();
+AuthorDataAccess ADA=new AuthorDataAccess();
+ADA.saveAuthor(1, author);
+
+Periodical periodical=new Periodical();
+periodicalDataAccess PDA =new periodicalDataAccess();
+PDA.savePeriodival(1, periodical);
+
 	}
 
 }
