ce52cf62903f66929a97d847ca184111af127600
985009 <985009@cslab_l209-101.cs.mum.edu>
2016-01-20 17:54:15 -0600

    add MainWindow

--- jazsystem/src/UI/LoginController.java
+++ jazsystem/src/UI/LoginController.java
@@ -1,9 +1,12 @@
 package UI;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class LoginController {
 
@@ -13,8 +16,24 @@ public class LoginController {
 	@FXML
 	private PasswordField passwordField;
 
+	private Stage loginStage;
+	private Stage mainStage;
 
 	public void handleLoginButton(ActionEvent event){
-		usernameField.setText(passwordField.getText());
+		loginStage.hide();
+		mainStage.show();
 	}
+
+	public void handleExitButton(ActionEvent event){
+		Platform.exit();
+	}
+
+	public void setLoginStage(Stage loginStage) {
+		this.loginStage = loginStage;
+	}
+
+	public void setMainStage(Stage mainStage) {
+		this.mainStage = mainStage;
+	}
+
 }
--- jazsystem/src/UI/Main.java
+++ jazsystem/src/UI/Main.java
@@ -8,6 +8,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 
 
 public class Main extends Application {
@@ -15,12 +16,23 @@ public class Main extends Application {
 	private Logger logger = Logger.getLogger(Main.class.getName());
 
 	@Override
-	public void start(Stage primaryStage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
-		primaryStage.setTitle("jaz Library System");
-		primaryStage.setScene(new Scene(root, 500, 350));
-		primaryStage.setResizable(false);
-		primaryStage.show();
+	public void start(Stage loginStage) throws Exception {
+		FXMLLoader loginLoader = new FXMLLoader(getClass().getResource("Login.fxml"));
+		Parent loginPane = loginLoader.load();
+		LoginController loginController = loginLoader.getController();
+		FXMLLoader mainWindowLoader = new FXMLLoader(getClass().getResource("MainWindow.fxml"));
+		Parent mainPane = mainWindowLoader.load();
+
+		loginStage.setTitle("jaz Library System");
+		loginStage.setScene(new Scene(loginPane, 500, 350));
+		loginStage.setResizable(false);
+		Stage mainStage = new Stage();
+		mainStage.setTitle("Welcome to jaz Library System");
+		mainStage.setScene(new Scene(mainPane, 800, 600));
+		mainStage.setResizable(false);
+		loginController.setLoginStage(loginStage);
+		loginController.setMainStage(mainStage);
+		loginStage.show();
 //		logger.log(Level.INFO, "test logging...");
 	}
 
