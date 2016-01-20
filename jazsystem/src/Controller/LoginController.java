b2b2ab2e95e090e1dc123530219e8a67809ac408
985009 <985009@cslab_l209-101.cs.mum.edu>
2016-01-20 10:24:19 -0600

    adjust Login UI

--- jazsystem/src/Controller/LoginController.java
+++ jazsystem/src/Controller/LoginController.java
@@ -1,5 +1,16 @@
 package Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+
+	@FXML
+	private TextField usernameField;
+
+	@FXML
+	private PasswordField passwordField;
+
 
 }
